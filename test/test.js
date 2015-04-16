bcec68be8b79e81244147f6d3860d285dcb7cc9d
why520crazy <why520crazy@163.com>
2015-04-16 15:10:52 +0800

    add winston-mongodb for worktile

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var winston = require('winston');
+
+/**
+ * Requiring `winston-mongodb` will expose
+ * `winston.transports.MongoDB`
+ */
+var MongoDB = require('../lib/winston-mongodb-wt').MongoDB;
+
+winston.add(MongoDB, {
+    label: 'wtweb', db: "mongodb://localhost:27017/wtlog", errorCollection: "error"
+});
+
+winston.info("哈哈 我是info", {op: 123});
+winston.info("哈哈 我是info1", "dddd", {op: 123});
+
+winston.error(new Error("的确报错了"), "哈哈 error", {op: 123});
+
+winston.error("哈哈 error1", {op: 123});
+
+winston.error("哈哈 error2", "eeee", {op: 123});
