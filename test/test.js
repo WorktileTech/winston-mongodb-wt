f38826130c23733feea5e22c915f1a02c01b9e4b
why520crazy <why520crazy@163.com>
2015-08-05 11:34:05 +0800

    upgrade to 1.0.9  fix connect pool bug

--- test/test.js
+++ test/test.js
@@ -15,8 +15,6 @@ winston.add(MongoDB, {
 winston.info("哈哈 我是info", {op: 123});
 winston.info("哈哈 我是info1", "dddd", {op: 123});
 
-winston.error(new Error("的确报错了"), "哈哈 error", {op: 123});
-
 winston.error("哈哈 error1", {op: 123});
 
 winston.error("哈哈 error2", "eeee", {op: 123});
