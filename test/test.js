c38196e96ed49e350925575842b1ae5e81be4378
why520crazy <why520crazy@163.com>
2016-02-29 16:15:25 +0800

    upgrade winston mongodb

--- test/test.js
+++ test/test.js
@@ -6,15 +6,18 @@ var winston = require('winston');
  */
 var MongoDB = require('../lib/winston-mongodb-wt').MongoDB;
 
-winston.add(MongoDB, {
+var transport = new MongoDB({
     label          : 'wtweb',
     db             : "mongodb://localhost:27017/wtlog",
     errorCollection: "error"
 });
+winston.add(transport, {}, true);
 
 winston.info("哈哈 我是info", {op: 123});
-winston.info("哈哈 我是info1", "dddd", {op: 123,oid:1111});
+winston.info("哈哈 我是info1", "dddd", {op: 123, oid: 1111});
 
 winston.error("哈哈 error1", {op: 123});
 
-winston.error("哈哈 error2", "eeee", {op: 123,oid:222});
+winston.error("哈哈 error2", "eeee", {op: 123, oid: 222});
+
+transport.addDuration("api 测试", 30);
